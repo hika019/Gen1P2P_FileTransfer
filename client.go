4c98eec58b0036d901dec6bef2aed551be4c427a
hika019 <ikazuti2001@gmail.com>
2022-07-17 20:35:52 +0900

    ローカルでのハッシュテーブルの作成

--- client.go
+++ client.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"Gen1P2P_FileTransfer/lib"
+	"crypto/sha256"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 )
 
 var shareDir string
+var hashTable map[[32]byte]string
 
 var conf lib.ConfigStruct
 
@@ -18,6 +22,9 @@ func init() {
 		err := os.Mkdir(shareDir, 0777)
 		lib.CheckErrExit(err)
 	}
+
+	hashTable = map[[32]byte]string{}
+	uploadFileHashTable()
 
 }
 
@@ -31,10 +38,50 @@ func loadConf() {
 	shareDir = conf.Share_dir_name
 }
 
-func send() {
+func loadLocalFiles(dir string) []string {
+	files, err := ioutil.ReadDir(dir)
+	lib.CheckErrExit(err)
+
+	var paths []string
+
+	for _, file := range files {
+		if file.IsDir() {
+			//paths = append(paths, loadLocalFiles(filepath.Join(dir, file.Name()))...)
+			continue
+		}
+		paths = append(paths, filepath.Join("", file.Name()))
+	}
+	return paths
+}
+
+func createFileHashTable(dir string) {
+
+	paths := loadLocalFiles(dir)
+
+	for _, path := range paths {
+		h := sha256.Sum256([]byte(path))
+		hashTable[h] = path
+	}
+}
+
+func uploadFileHashTable() {
+	createFileHashTable(shareDir)
+	var hashs []byte
+	for hash := range hashTable {
+		for i := 0; i < 32; i++ {
+			hashs = append(hashs, hash[i])
+		}
+
+	}
+	fmt.Println(hashs)
+	fmt.Println(len(hashs))
 
 }
 
-func receive() {
+func uploadFile() {
+
+}
+
+func downloadFile() {
 
 }
